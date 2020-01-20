430075c05c46a87dc7ec0a0ee0949c9181329bea
Marlonego <negoscapo@gmail.com>
2020-01-20 11:47:18 +0100

    encapsulate the js code

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+(
+    () => {
+        const randomNumber = Math.floor(Math.random() * 10 + 1);
+        const userNumber = prompt("Guess a number between 1 - 10");
 
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-const userNumber = prompt("Guess a number between 1 - 10");
-
-if(parseInt(userNumber) === randomNumber) {
-    alert(`Success, the number was ${userNumber}`);
-} else {
-    alert('Wrong, try again!');
-}
+        if (parseInt(userNumber) === randomNumber) {
+            alert(`Success, the number was ${userNumber}`);
+        } else {
+            alert('Wrong, try again!');
+        }
+    }
+)();
