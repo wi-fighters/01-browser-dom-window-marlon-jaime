e28c73ce1b6be2c30fc56b10aa0bc176e774efc7
Jaime Segura <jseguraweb@gmail.com>
2020-01-20 14:11:26 +0100

    second try

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-(
-    () => {
-        const randomNumber = Math.floor(Math.random() * 10 + 1);
-        const userNumber = prompt("Guess a number between 1 - 10");
+const randomNumber = Math.floor(Math.random() * 10 + 1); // 1
 
-        if (parseInt(userNumber) === randomNumber) {
-            alert(`Success, the number was ${userNumber}`);
+console.log(`....................................${randomNumber}`);
+
+const promptForGuesses = number => {
+
+    let attempts = 0; // 2
+
+    let userNumber = prompt("Guess a number between 1 and 10"); // 2
+
+    while(attempts < 3){
+        attempts =+ 1;
+
+        if(parseInt(userNumber) === randomNumber) {
+            attempts = 3;
+            alert(`Success! The number was ${randomNumber}`);
         } else {
-            alert('Wrong, try again!');
+            attempts =+ 1;
+            userNumber = prompt("Nope, sorry, try again"); // 3
         }
     }
-)();
+}
+
+promptForGuesses(randomNumber);
