3a22495b4ed534ec3e72f20c84b812f5bddc0791
Jaime Segura <jseguraweb@gmail.com>
2020-01-20 11:43:33 +0100

    add variables and if statement

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const userNumber = prompt("Guess a number between 1 - 10");
+
+if(parseInt(userNumber) === randomNumber) {
+    alert(`Success, the number was ${userNumber}`);
+} else {
+    alert('Wrong, try again!');
+}
