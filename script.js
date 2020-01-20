3783de213356e034497e3b0caa9bb5ee48d220d9
Jaime Segura <jseguraweb@gmail.com>
2020-01-20 17:15:48 +0100

    solve parseint part

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 (()=> {
-    const randomNumber = Math.floor(Math.random() * 10 + 1); // 1
-
+    const randomNumber = Math.floor(Math.random() * 10) + 1; // 
     console.log(`....................................${randomNumber}`);
 
     const promptForGuesses = number => {
-
         let attempts = 0; // 1
-
-        let userNumber = prompt("Guess a number between 1 and 10"); // 5
-
-        if(parseInt(userNumber) === randomNumber) {
-            alert(`Success! The number was ${randomNumber}`);
-
-        } else {
-            while(attempts < 3){
-                if(parseInt(userNumber) !== randomNumber){
-                userNumber = prompt("Nope, sorry, try again");
-                }
-            }
-            alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber}!`);
+        let userNumber = prompt("Guess a number between 1 and 10"); // 
+        
+        if(userNumber === null) {
+            return;
         }
-        attempts =+ 1;
+
+        if (attempts < 3){
+            userNumber = parseInt(userNumber, 10);
+            attempts += 1;
+
+            if(userNumber === randomNumber){
+                alert(`Success! The number was ${randomNumber}! Attempts: ${attempts}`);
+                return;
+            } else {
+                userNumber = prompt("Nope, try again!");
+                return;
+            }
+        } else {
+            alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber}!`);
+            return;
+        }
     };
 
-
     promptForGuesses(randomNumber);
+
 })();
