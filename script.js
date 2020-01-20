dbc51a9d6b8fd7cd34a1a0ca8008784fd92349eb
Jaime Segura <jseguraweb@gmail.com>
2020-01-20 20:59:29 +0100

    add correct solution

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 (()=> {
-    const randomNumber = Math.floor(Math.random() * 10) + 1;
-
-    const promptForGuesses = number => {
-        let attempts = 0;
-        let userNumber = prompt("Guess a number between 1 and 10"); // 
-        
-        if(userNumber === null) {
+    const randomNumber = (Math.floor(Math.random() * 10) + 1).toString();
+    console.log(randomNumber);
+    let userNumber = prompt("Guess a number between 1 and 10");
+    let attempts = 1;
+            
+    while (attempts < 3) { 
+        if (userNumber === null) {
             return;
         }
-
-        for(let i = 3; i > attempts; i--){        
-                userNumber = parseInt(userNumber, 10);
-                attempts += 1;
-
-                if(userNumber === randomNumber){
-                    console.log("SUCCESS");
-                    alert(`Success! The number was ${randomNumber}! Attempts: ${attempts}`);
-                    return;
-                } else {
-                    console.log("WRONG");
-                    userNumber = prompt("Nope, try again!");
-                }
+        if (userNumber !== randomNumber) {
+            userNumber = prompt("Nope, try again!");
+            attempts += 1;
         }
-        
-        alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber}!`);
-        return;
-    };
-
-    promptForGuesses(randomNumber);
-
+        if (userNumber === randomNumber){
+            return alert(`Success! The number was ${randomNumber}! Attempts: ${attempts}`);
+        }        
+    }
+    return alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber}!`);
 })();
