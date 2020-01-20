935880e3a41bee73df1a98d7431d5d9de6cf9881
Jaime Segura <jseguraweb@gmail.com>
2020-01-20 17:28:22 +0100

    solve exercise!

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 (()=> {
-    const randomNumber = Math.floor(Math.random() * 10) + 1; // 
-    console.log(`....................................${randomNumber}`);
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
 
     const promptForGuesses = number => {
-        let attempts = 0; // 1
+        let attempts = 0;
         let userNumber = prompt("Guess a number between 1 and 10"); // 
         
         if(userNumber === null) {
             return;
         }
 
-        if (attempts < 3){
-            userNumber = parseInt(userNumber, 10);
-            attempts += 1;
+        for(let i = 3; i > attempts; i--){        
+                userNumber = parseInt(userNumber, 10);
+                attempts += 1;
 
-            if(userNumber === randomNumber){
-                alert(`Success! The number was ${randomNumber}! Attempts: ${attempts}`);
-                return;
-            } else {
-                userNumber = prompt("Nope, try again!");
-                return;
-            }
-        } else {
-            alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber}!`);
-            return;
+                if(userNumber === randomNumber){
+                    console.log("SUCCESS");
+                    alert(`Success! The number was ${randomNumber}! Attempts: ${attempts}`);
+                    return;
+                } else {
+                    console.log("WRONG");
+                    userNumber = prompt("Nope, try again!");
+                }
         }
+        
+        alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber}!`);
+        return;
     };
 
     promptForGuesses(randomNumber);
